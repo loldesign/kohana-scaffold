7ec21bc6ab75a6d6002bcf4ef7e141d0c01a5fd7
Marco Singer <markaum@gmail.com>
2013-06-27 03:34:44 -0300

    added a Silent Mode to AppGenerator

--- lib/kohana-scaffold/generators/app/app_generator.rb
+++ lib/kohana-scaffold/generators/app/app_generator.rb
@@ -11,6 +11,11 @@ module KohanaScaffold
       aliases: '-p',
       desc: 'Define the Apache public directory'
 
+    class_option :silent,
+      type: :boolean,
+      aliases: '-V',
+      desc: 'Silent mode'
+
     def initialize(args=[], options={}, config={})
       super(args, options, config)
       self.destination_root = "#{@options[:public_path]}/"
@@ -21,20 +26,30 @@ module KohanaScaffold
     end
 
     def copy_kohana
-      directory("kohana", project_name)
+      directory("kohana", project_name, default_options)
     end
 
     def create_cache_folder
-      empty_directory("#{project_name}/application/cache", verbose: false)
+      empty_directory("#{project_name}/application/cache", default_options)
     end
 
     def create_bootstrap
-      template("bootstrap.erb", "#{project_name}/application/bootstrap.php")
+      template("bootstrap.erb", "#{project_name}/application/bootstrap.php", default_options)
     end
 
     def add_default_cookie_salt
+      options = default_options.merge(
+        {after: "I18n::lang('en-us');\n"}
+      )
+
       cookie_salt = "Cookie::$salt = '#{SecureRandom.uuid}';"
-      inject_into_file("#{project_name}/application/bootstrap.php", cookie_salt, after: "I18n::lang('en-us');\n")
+      inject_into_file("#{project_name}/application/bootstrap.php", cookie_salt, options)
+    end
+
+    private
+
+    def default_options
+      @default_options ||= {verbose: @options[:silent].nil?}
     end
   end
 end
